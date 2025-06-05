4d7eeab8c9548346fcba598501bec641e045ed1c
AniketPrajapati <aniketprajapati168@gmail.com>
2025-06-05 21:00:20 +0530

    small_update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/animation.dart';
 
 void main() => runApp(WabricBlogApp());
 String _currentSubtitle = "WHITE SHIRT";
